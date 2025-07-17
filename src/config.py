328def983cce6d897e30243bf3d3bb1613e02e56
Amama-Fatima <amamafatima58@gmail.com>
2025-07-17 15:08:39 +0500

    debugging

--- src/config.py
+++ src/config.py
@@ -4,11 +4,12 @@ from httpx import Timeout
 
 load_dotenv()
 
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+
 GITHUB_HEADERS = {
     "Authorization": f"token {GITHUB_TOKEN}",
     "Accept": "application/vnd.github.v3+json"
-} if GITHUB_TOKEN else {}
+}
 
 TIMEOUT = Timeout(
     connect=15.0,
