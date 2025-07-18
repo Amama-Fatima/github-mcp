6397f787f6b38caa7a52ea68cc6a5a4800a3ce6e
Amama-Fatima <amamafatima58@gmail.com>
2025-07-18 12:40:09 +0500

    not raise an error in case github token is not present in the env

--- src/config.py
+++ src/config.py
@@ -4,12 +4,12 @@ from httpx import Timeout
 
 load_dotenv()
 
-GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
 GITHUB_HEADERS = {
     "Authorization": f"token {GITHUB_TOKEN}",
     "Accept": "application/vnd.github.v3+json"
-}
+} if GITHUB_TOKEN else {}
 
 TIMEOUT = Timeout(
     connect=15.0,
